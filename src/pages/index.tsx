4026ddf1599f6e076c3d0691779d623e4112555d
Narun Yadav <n.y.narun@gmail.com>
2022-07-19 20:46:32 +0530

    added gtag

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import { Navbar } from "../components";
 import { Header, About, Work, Skills, Footer } from "../container";
+import Script from "next/script";
 
 const Home: NextPage = () => {
   return (
@@ -15,6 +16,21 @@ const Home: NextPage = () => {
         />
         <meta name="theme-color" content="#e4e4e4" />
       </Head>
+      <div>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-N7CQJ7QBGK"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-N7CQJ7QBGK');
+        `}
+        </Script>
+      </div>
       <Navbar />
       <Header />
       <About />
